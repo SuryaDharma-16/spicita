034cd93e6b16feb37353cb3fe863e76b0df838b1
Surya Dharma <gedesurya405@gmail.com>
2024-12-10 11:30:29 +0800

    Menambahkan halaman penting

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,8 @@ import Logo from "../Assets/logo.svg";
 const Navbar = () => {
   return (
     <>
-      <div className="flex align-middle my-4 justify-between mx-8">
-        <img src={Logo} alt="Spicita" className="h-12 w-auto" />
+      <div className="flex align-middle my-4 justify-between mx-24">
+        <img src={Logo} alt="Spicita" className="h-8 w-auto" />
         <ul className="flex justify-end">
           <li className="mx-4">Beranda</li>
           <li className="mx-4">Dasbor Belajar</li>
---
+++ src/pages/Dasbor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dasbor = () => {
+  return (
+    <div>Dasbor</div>
+  )
+}
+
+export default Dasbor
