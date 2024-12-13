8a1499c2169e904810984c119e16e732a5f72253
Surya Dharma <gedesurya405@gmail.com>
2024-12-13 19:35:51 +0800

    Update Halaman Sertifikat

--- src/Components/Subsections/Stepper.jsx
+++ src/Components/Subsections/Stepper.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import Plus from '../../Assets/Plus'
-import Minus from '../../Assets/Minus'
+import React from 'react';
+import Plus from '../../Assets/Plus';
+import Minus from '../../Assets/Minus';
+import { useState } from 'react';
 
 const Stepper = () => {
-  const [value, setValue] = setValue('0');
+  const [value, setValue] = useState(1);
+
+  const incrementValue = () => {
+    setValue(prevValue => prevValue + 1);
+  };
+
+  const decrementValue = () => {
+    setValue(prevValue => (prevValue > 1 ? prevValue - 1 : 1));
+  };
 
   return (
     <div className='flex flex-row'>
       <div className='flex items-center border-2 border-yellow-950 rounded-tl-md rounded-bl-md'>
-        <button onClick={
-          ''
-        }><Plus fill={'#422006'}/></button>
+        <button onClick={decrementValue}>
+          <Minus fill={'#422006'} />
+        </button>
       </div>
       <div className='flex justify-center items-center border-y-2 border-yellow-950 h-8 w-12'>
-        <p>{}</p>
+        <p className='font-lexendDeca text-p text-yellow-950'>{value}</p>
       </div>
       <div className='flex items-center border-2 border-yellow-950 rounded-tr-md rounded-br-md'>
-        <button><Minus fill={'#422006'}/></button>
+        <button onClick={incrementValue}>
+          <Plus fill={'#422006'} />
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stepper
+export default Stepper;
--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -7,8 +7,9 @@ import Testi from '../Components/Sections/Testi'
 import Paper from '../Assets/certificate-paper.jpg'
 import Linkedin from '../Assets/linkedin.svg'
 import Stepper from '../Components/Subsections/Stepper'
+import Button from '../Components/Subsections/Button'
 
-const Certificate = () => {
+const Certificate = ({price}) => {
   return (
     <>
       <Nav1 />
@@ -18,14 +19,24 @@ const Certificate = () => {
           <img src={Paper} alt="certificate" className='rounded-md'/>
         </div>
         <div className='w-[calc((50vw-6rem))]'>
-        <p className='font-lexendDeca text-h5 text-yellow-950 font-bold mb-4'>Bagikan Sertifikat</p>
+        <p className='font-lexendDeca text-h5 text-yellow-950 font-bold mb-4'>Dapatkan & Bagikan Sertifikat</p>
           <div className='flex flex-row gap-4 mb-4'>
             <img src={Facebook} alt="facebook" className='h-8 w-auto'/>
             <img src={Instagram} alt="facebook" className='h-8 w-auto'/>
             <img src={Linkedin} alt="facebook" className='h-8 w-auto'/>
           </div>
-          <p className='font-prociono text-p text-yellow-950 mb-4'>"Siap untuk sertifikasi keterampilan ekspor dan hilirisasi produk rempah-rempah cengkeh yang terstandarisasi? Sertifikasi ini akan memvalidasi keterampilan berharga Anda dan dapat membuat Anda menonjol di pasar global. Pelatihan ini cocok untuk pemula maupun profesional di industri."</p>
-          <Stepper/>
+          <p className='font-prociono text-p text-yellow-950 mb-4'>Siap untuk sertifikasi keterampilan ekspor dan hilirisasi produk rempah-rempah cengkeh yang terstandarisasi? Sertifikasi ini akan memvalidasi keterampilan berharga Anda dan dapat membuat Anda menonjol di pasar global.</p>
+          <div className='flex flex-row mb-4'>
+            <p className='font-lexendDeca text-h5 text-primary'>Rp.</p>
+            <p className='font-lexendDeca text-h5 text-primary'>139.000,00</p>
+          </div>
+          <div className='flex flex-wrap gap-4'>
+            <Stepper />
+            <Button
+            link={'/App'}
+            label={'Dapatkan Sertifikat'}
+            />
+          </div>
         </div>
       </div>
       <Testi/>
