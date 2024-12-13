639fa8bf7ae5f945a38c3387ba9c61c9f674d27c
Surya Dharma <gedesurya405@gmail.com>
2024-12-13 14:18:12 +0800

    Menambahkan halaman sertifikat

--- src/Components/Subsections/Breadcrumbs.jsx
+++ src/Components/Subsections/Breadcrumbs.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Breadcrumbs = ({title}) => {
+const Breadcrumbs = ({ title }) => {
   return (
     <>
-      <p className="font-lexendDeca text-small text-gray-500">Dasbor Belajar</p>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="size-6"
-      >
-        <path
-          fill="#422006"
-          fillRule="evenodd"
-          d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z"
-          clipRule="evenodd"
-        />
-      </svg>
-      <p className="font-lexendDeca text-yellow-950 text-small">{title}</p>
+      <div className="flex flex-row gap-[4px] justify-start items-center">
+        <p className="font-lexendDeca text-p text-gray-500">
+          <Link to="/dashboard">Dasbor Belajar</Link>
+        </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={3}
+          stroke="currentColor"
+          className="size-4"
+        >
+          <path
+            color="#422006"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          />
+        </svg>
+
+        <p className="font-lexendDeca text-yellow-950 text-p">{title}</p>
+      </div>
     </>
   );
 };
